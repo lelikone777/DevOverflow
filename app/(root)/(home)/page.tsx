4d1b6a341f4db6948e8defbf0bb682f5edebe835
lelikone777 <2257855@gmail.com>
2023-10-11 18:32:29 +0400

    Home Filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,8 @@
+import HomeFilters from '@/components/home/HomeFilters'
+import Filter from '@/components/shared/Filter'
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
 import { Button } from '@/components/ui/button'
+import { HomePageFilters } from '@/constants/filters'
 import Link from 'next/link'
 
 export default function Home () {
@@ -15,8 +19,12 @@ export default function Home () {
       </div>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar route="/" iconPosition="left" imgSrc="/assets/icons/search.svg" placeholder="Search for questions" otherClasses="flex-1" />
 
+        <Filter filters={HomePageFilters} otherClasses="min-h-[56px] sm:min-w-[170px]" containerClasses="hidden max-md:flex" />
       </div>
+
+      <HomeFilters />
 
     </>
   )
