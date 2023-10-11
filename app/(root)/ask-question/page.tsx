eebec97b452ebc6cc1afe0a248d41bbfaae0a9d4
lelikone777 <2257855@gmail.com>
2023-10-11 23:08:34 +0400

    create Form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,13 @@
+import Question from '@/components/forms/Question'
+
 const Page = () => {
   return (
-    <h1 className='h1-bold'>ask-question123</h1>
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+      <div className='mt-9'>
+        <Question />
+      </div>
+    </div>
   )
 }
 
