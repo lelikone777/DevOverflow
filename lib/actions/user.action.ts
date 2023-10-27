ca302f11f396853d3f3f0808bcaa8f51ab9291ae
lelikone777 <2257855@gmail.com>
2023-10-27 17:52:31 +0400

    user actions and routes

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,10 @@
 'use server'
 
 import User from '@/database/user.model'
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from '@/lib/actions/shared.types'
+import { revalidatePath } from 'next/cache'
 import { connectToDatabase } from '../mongoose'
+import Question from '@/database/question.model'
 
 export async function getUserById (params: any) {
   try {
@@ -9,6 +12,59 @@ export async function getUserById (params: any) {
     const { userId } = params
     const user = await User.findOne({ clerkId: userId })
     return user
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createUser (userData: CreateUserParams) {
+  try {
+    await connectToDatabase()
+    const newUser = await User.create(userData)
+    return newUser
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function updateUser (params: UpdateUserParams) {
+  try {
+    await connectToDatabase()
+    const { clerkId, updateData, path } = params
+    await User.findOneAndUpdate(
+      { clerkId },
+      updateData,
+      { new: true })
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteUser (params: DeleteUserParams) {
+  try {
+    await connectToDatabase()
+    const { clerkId } = params
+    const user = await User.findOneAndDelete({ clerkId })
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    // Delete user from MongoDB
+    // delete questions, answers, comments, etc.
+
+    // get user questions ids
+    const userQuestionIds = await Question.find({ author: user._id }).distinct('_id')
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id })
+
+    // TODO: delete user answers, comments, etc.
+    const deletedUser = await User.findByIdAndDelete(user._id)
+    return deletedUser
   } catch (error) {
     console.log(error)
     throw error
