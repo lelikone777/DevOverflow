466950fabbac0f0f2858af16010646a21c5098e8
lelikone777 <2257855@gmail.com>
2023-10-31 01:19:40 +0400

    manage search state

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -18,6 +18,7 @@ import { Button } from '../ui/button'
 import { QuestionsSchema } from '@/lib/validations'
 import { Badge } from '../ui/badge'
 import Image from 'next/image'
+
 import { useRouter, usePathname } from 'next/navigation'
 import { useTheme } from '@/context/ThemeProvider'
 import { createQuestion, editQuestion } from '@/lib/actions/question.actions'
@@ -35,24 +36,17 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
   const router = useRouter()
   const pathname = usePathname()
 
-  // const parsedQuestionDetails = JSON.parse(questionDetails || '')
-  //
-  // const groupedTags = parsedQuestionDetails.tags.map((tag) => tag.name)
+  const parsedQuestionDetails =
+    questionDetails && JSON.parse(questionDetails || '')
 
-  const parsedQuestionDetails = questionDetails
-    ? JSON.parse(questionDetails)
-    : ''
-
-  const groupedTags = parsedQuestionDetails
-    ? parsedQuestionDetails.tags.map((tag) => tag.name)
-    : []
+  const groupedTags = parsedQuestionDetails?.tags.map((tag) => tag.name)
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
-      title: parsedQuestionDetails.title || '',
-      explanation: parsedQuestionDetails.content || '',
+      title: parsedQuestionDetails?.title || '',
+      explanation: parsedQuestionDetails?.content || '',
       tags: groupedTags || [],
     },
   })
@@ -169,7 +163,7 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
                   }}
                   onBlur={field.onBlur}
                   onEditorChange={(content) => field.onChange(content)}
-                  initialValue={parsedQuestionDetails.content || ''}
+                  initialValue={parsedQuestionDetails?.content || ''}
                   init={{
                     height: 350,
                     menubar: false,
