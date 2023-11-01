28c707c757da8cea2e916919ef15a31b032cb2b7
lelikone777 <2257855@gmail.com>
2023-11-01 17:09:53 +0400

    local search

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -23,6 +23,8 @@ const AllAnswers = async ({
 }: Props) => {
   const result = await getAnswers({
     questionId,
+    page: page ? +page : 1,
+    sortBy: filter,
   })
 
   return (
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,43 +63,11 @@ export async function updateUser(params: UpdateUserParams) {
   }
 }
 
-export async function deleteUser(params: DeleteUserParams) {
-  try {
-    connectToDatabase()
-
-    const { clerkId } = params
-
-    const user = await User.findOneAndDelete({ clerkId })
-
-    if (!user) {
-      throw new Error('User not found')
-    }
-
-    // Delete user from database
-    // and questions, answers, comments, etc.
-
-    // get user question ids
-    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
-
-    // delete user questions
-    await Question.deleteMany({ author: user._id })
-
-    // TODO: delete user answers, comments, etc.
-
-    const deletedUser = await User.findByIdAndDelete(user._id)
-
-    return deletedUser
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
-}
-
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase()
 
-    const { searchQuery } = params
+    const { searchQuery, filter } = params
 
     const query: FilterQuery<typeof User> = {}
 
@@ -110,7 +78,24 @@ export async function getAllUsers(params: GetAllUsersParams) {
       ]
     }
 
-    const users = await User.find(query).sort({ createdAt: -1 })
+    let sortOptions = {}
+
+    switch (filter) {
+      case 'new_users':
+        sortOptions = { joinedAt: -1 }
+        break
+      case 'old_users':
+        sortOptions = { joinedAt: 1 }
+        break
+      case 'top_contributors':
+        sortOptions = { reputation: -1 }
+        break
+
+      default:
+        break
+    }
+
+    const users = await User.find(query).sort(sortOptions)
 
     return { users }
   } catch (error) {
@@ -160,17 +145,40 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase()
 
-    const { clerkId, searchQuery } = params
+    const { clerkId, searchQuery, filter } = params
 
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, 'i') } }
       : {}
 
+    let sortOptions = {}
+
+    switch (filter) {
+      case 'most_recent':
+        sortOptions = { createdAt: -1 }
+        break
+      case 'oldest':
+        sortOptions = { createdAt: 1 }
+        break
+      case 'most_voted':
+        sortOptions = { upvotes: -1 }
+        break
+      case 'most_viewed':
+        sortOptions = { views: -1 }
+        break
+      case 'most_answered':
+        sortOptions = { answers: -1 }
+        break
+
+      default:
+        break
+    }
+
     const user = await User.findOne({ clerkId }).populate({
       path: 'saved',
       match: query,
       options: {
-        sort: { createdAt: -1 },
+        sort: sortOptions,
       },
       populate: [
         { path: 'tags', model: Tag, select: '_id name' },
